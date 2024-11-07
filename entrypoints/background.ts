279703d5df0a72d21d57444319cfd3f23f3017eb
liuxingyu521 <xuj_power@qq.com>
2024-11-07 19:04:39 +0800

    feat: use document.elementsFromPoint to check img element

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,16 +1,38 @@
 export default defineBackground(() => {
+  let currentImageUrl: string | null = null;
+
+  const toggleMenuVisible = (visible: boolean) => {
+    browser.contextMenus.update('viewImage', {
+      visible,
+    });
+  }
+
   browser.runtime.onInstalled.addListener(() => {
     browser.contextMenus.create({
       id: "viewImage",
       title: "查看大图",
-      contexts: ["image"]
+      visible: false
     });
-  });
-  
+  })
+
   browser.contextMenus.onClicked.addListener((info, tab) => {
     if (info.menuItemId === "viewImage" && tab?.id) {
-      browser.tabs.sendMessage(tab.id, { url: info.srcUrl });
+      browser.tabs.sendMessage(tab.id, { type: 'PREVIEW_IMG', url: currentImageUrl });
+
+      toggleMenuVisible(false)
     }
   });
 
+  // 监听来自 content script 的消息
+  browser.runtime.onMessage.addListener((message) => {
+    if (message.type === 'RIGHT_CLICK_IMAGE') {
+      currentImageUrl = message.url;
+
+      toggleMenuVisible(true)
+    }
+    
+    if (message.type === 'HIDE_PREVIEW_LARGE_IMG') {
+      toggleMenuVisible(false)
+    }
+  });
 });
--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -4,11 +4,11 @@ import '@/assets/image-viewer.min.js'
 export default defineContentScript({
   matches: ['<all_urls>'],
   main() {
-    browser.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
-      if (message.url) {
+    browser.runtime.onMessage.addListener(async (message) => {
+      if (message.type === 'PREVIEW_IMG' && message.url) {
         const imageDom = document.createElement('img');
         imageDom.src = message.url;
-        
+
         // @ts-expect-error global variable
         new Viewer(imageDom, {
           navbar: false,
@@ -20,6 +20,28 @@ export default defineContentScript({
         });
         imageDom.click();
       }
+    });
+
+    document.addEventListener('contextmenu', async (event) => {
+      // 获取点击位置路径上的所有元素
+      const allElementsAtCurPoint = document.elementsFromPoint(event.pageX, event.pageY);
+      // 找到有 src 属性的图片元素
+      const targetImg = allElementsAtCurPoint.find(item => item.tagName === 'IMG' && item.getAttribute('src'))
+
+      if(!targetImg) {
+        browser.runtime.sendMessage({
+          type: 'HIDE_PREVIEW_LARGE_IMG'
+        })
+        return;
+      }
+
+      const targetImgUrl = targetImg.getAttribute('src');
+
+      // 注册右键菜单
+      browser.runtime.sendMessage({
+        type: 'RIGHT_CLICK_IMAGE',
+        url: targetImgUrl
+      });
     });
   },
 });
